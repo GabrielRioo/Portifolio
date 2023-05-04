75c51783b2def5c042956ae2efa917e130499cfc
GabrielRioo <gabrielpaivario@gmail.com>
2023-05-04 15:54:11 -0300

    adding the previosly content of the page principal page

--- Portifolio/src/App.jsx
+++ Portifolio/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import Sidebar from './components/sidebar'
+import Box from './components/box'
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Sidebar />
-    </>
+      <div className="content">
+        <h1>Headline aqui</h1>
+
+        <div className="content_options"> 
+          <Box />
+          <Box />
+          <Box />
+        </div>
+
+        <button className="contato">clique aqui</button>
+      </div>
+    </div>
   )
 }
 
---
+++ Portifolio/src/components/box.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BOX_STYLE = {
+    width: '350px',
+    height: '350px',
+    backgroundColor: 'red'
+}
+
+export default function Box() {
+    return (
+        <div style={BOX_STYLE}>
+            
+        </div>
+    )
+}
